9fb5c9d9e8366ca068d794a3f618e5a9f648c193
SantiagoF311 <112028544+SantiagoF311@users.noreply.github.com>
2023-05-02 18:14:22 -0500

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+const Btn = document.querySelectorAll ('button')
+const BtnStart = Btn[0];
+const BtnStop = Btn[1];
+
+var interval;
+
+function start() {
+  interval = setInterval(function() {
+    var color = generarColorAleatorio();
+    document.body.style.backgroundColor = color;
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(interval);
+}
+
+function generarColorAleatorio() {
+  var letras = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letras[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+BtnStart.addEventListener('click', start);
+BtnStop.addEventListener('click', stop)
